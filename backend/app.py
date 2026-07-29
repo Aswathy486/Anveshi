65ee171f64fe89059bad420e4b1235783a651773
Aswathy Santhosh <aswathysanthosh7025@gmail.com>
2026-07-30 00:05:37 +0530

    Added backend live url

--- backend/app.py
+++ backend/app.py
@@ -15,6 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://anveshi-backend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
