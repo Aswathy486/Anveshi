ce2b8317c89206ebaccd0df8fb52101a26c8ba57
Aswathy Santhosh <aswathysanthosh7025@gmail.com>
2026-07-30 00:23:00 +0530

    Fix CORS origin

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://anveshi.vercel.app/",
+        "https://anveshi.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
